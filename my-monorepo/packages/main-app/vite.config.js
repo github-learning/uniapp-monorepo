d80f7aac6248417399ee5c70316ba1afb30d4fd4
zhaojiangbing <zhaojiangbing@123.com>
2024-09-06 16:07:20 +0800

    feat: main-app input sub-app config

--- my-monorepo/packages/main-app/vite.config.js
+++ my-monorepo/packages/main-app/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
+import { defineConfig } from "vite";
+import uni from "@dcloudio/vite-plugin-uni";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    uni(),
-  ],
-})
+  plugins: [uni()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "src/main.ts",
+        subApp1: "../sub-app-1/src/main.ts", // 子应用1
+        subApp2: "../sub-app-2/src/main.ts", // 子应用2
+      },
+    },
+  },
+});
